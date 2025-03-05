086802512b6a27b7b41e056a017e15ab2443cc87
root <root@DF-VM.mshome.net>
2025-03-05 16:23:17 +0800

    modify name

--- zerozero-deploy.groovy
+++ zerozero-deploy.groovy
@@ -14,22 +14,9 @@ pipeline {
                 dir('zerozero-g2-react-develop') {
                     sh 'npm install && npm install --save-dev assets-webpack-plugin@3.9.12 && npm run build:stage'
                     sh 'tar -czvf zerozero-build.tar.gz dist/ public/ config/ node_modules/'
-                    sshPublisher(publishers: [sshPublisherDesc(configName: 'Deploy-VM', sshCredentials: [encryptedPassphrase: '{AQAAABAAAAAgrfmDFjmGzgYrhZO4VmbdXvHeersemqOnXrv18rpHKZGc462V8tNFVqAaCKT9E3oA}', key: '', keyPath: '', username: 'root'], transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'tar -xvf zerozero-build.tar.gz', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: 'zerozero-build.tar.gz')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
+                    sshPublisher(publishers: [sshPublisherDesc(configName: 'Deploy-VM', sshCredentials: [encryptedPassphrase: '{AQAAABAAAAAgrfmDFjmGzgYrhZO4VmbdXvHeersemqOnXrv18rpHKZGc462V8tNFVqAaCKT9E3oA}', key: '', keyPath: '', username: 'root'], transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'rm -rf app/* && tar -xvf zerozero-build.tar.gz -C app && sh run.sh', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: 'zerozero-build.tar.gz')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
                 }
            }
-        }
-        stage('Deploy app') {
-            steps {
-                script {
-                    def remote = [:]
-                    remote.name = 'Deploy-VM'
-                    remote.host = '172.19.117.190'
-                    remote.user = 'root'
-                    remote.password = 'Tifalockheart@0215'  // 或使用密鑰
-                    remote.allowAnyHosts = true
-                }
-                sshCommand remote: remote, command: "ls -lrt"
-            }
         }
     }
 }
