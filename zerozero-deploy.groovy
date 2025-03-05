de1f2b90ea7adfcd5194d962d907e50482c3be13
root <root@DF-VM.mshome.net>
2025-03-05 12:39:52 +0800

    modify name

--- zerozero-deploy.groovy
+++ zerozero-deploy.groovy
@@ -12,6 +12,7 @@ pipeline {
            steps {
                 dir('zerozero-g2-react-develop') {
                     sh 'npm install && npm install --save-dev assets-webpack-plugin@3.9.12 && npm run build:stage'
+                    sh 'tar -czvf zerozero-build.tar.gz dist/ public/ config/ node_moudles/'
                 }
            }
         }
