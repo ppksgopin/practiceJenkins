fe0a4d3c58fa954031a2ac0df4defafe5a4bda72
root <root@DF-VM.mshome.net>
2025-03-05 08:56:15 +0800

    modify file

--- zeropos-deploy.groovy
+++ zeropos-deploy.groovy
@@ -1,14 +1,23 @@
 pipeline {
-    agent {
-        docker {
-            image 'node:14.21-slim'
-            args '-v /var/run/docker.sock:/var/run/docker.sock'
-        }
+    agent any
+
+    environment {
+        DOCKER_HOST = "unix:///var/run/docker.sock"
     }
+
     stages {
-        stage('Test') {
+        stage('Build') {
+            agent {
+                docker {
+                    image 'docker:24.0-cli' // 使用帶有 Docker 客戶端的官方鏡像
+                    args '-v /var/run/docker.sock:/var/run/docker.sock'
+                }
+            }
             steps {
-                sh 'echo "test text."'
+                script {
+                    sh 'docker pull node:14.21-slim'
+                    sh 'docker images'
+                }
             }
         }
     }
