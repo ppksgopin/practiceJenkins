08dc21df0f62bb2da570b1c40ed27696d0f6e142
root <root@DF-VM.mshome.net>
2025-03-05 12:48:15 +0800

    modify name

--- zerozero-deploy.groovy
+++ zerozero-deploy.groovy
@@ -12,9 +12,14 @@ pipeline {
            steps {
                 dir('zerozero-g2-react-develop') {
                     sh 'npm install && npm install --save-dev assets-webpack-plugin@3.9.12 && npm run build:stage'
-                    sh 'tar -czvf zerozero-build.tar.gz dist/ public/ config/ node_moudles/'
+                    sh 'tar -czvf zerozero-build.tar.gz dist/ public/ config/ node_modules/'
                 }
            }
+        }
+        stage('Transfer file'){
+            steps {
+                sshPublisher(publishers: [sshPublisherDesc(configName: 'Deploy-VM', sshCredentials: [encryptedPassphrase: '{AQAAABAAAAAgA1tVXvjP2qbJJ54rEm9OlNhbWtSUJRVHVD7BydIxVJWQDz+IP6lasW52JhDm0Pfm}', key: '', keyPath: '', username: 'root'], transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: 'zerozero-build.tar.gz')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
+            }
         }
     }
 }
