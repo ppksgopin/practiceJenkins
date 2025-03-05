a674929b00193ed8056c8660a7a3e9cf256e8642
root <root@DF-VM.mshome.net>
2025-03-05 10:28:12 +0800

    modify name

--- zerozero-deploy.groovy
+++ zerozero-deploy.groovy
@@ -1,24 +1,12 @@
 pipeline {
     agent any
 
-    environment {
-        DOCKER_HOST = "unix:///var/run/docker.sock"
-    }
-
     stages {
         stage('Build') {
-            agent {
-                docker {
-                    image 'docker:24.0-cli' // 使用帶有 Docker 客戶端的官方鏡像
-                    args '-v /var/run/docker.sock:/var/run/docker.sock'
-                }
-            }
-            steps {
-                script {
-                    sh 'docker pull node:14.21-slim'
-                    sh 'docker images'
-                }
-            }
+           steps {
+                sh 'pwd && ls -al'
+		
+           }
         }
     }
 }
