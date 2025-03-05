2bf37dcc3fb97b808523664247f32fa515d4109f
root <root@DF-VM.mshome.net>
2025-03-05 11:42:27 +0800

    modify name

--- zerozero-deploy.groovy
+++ zerozero-deploy.groovy
@@ -10,7 +10,7 @@ pipeline {
             }
            steps {
                 dir('zerozero-g2-react-develop') {
-                    sh 'sudo npm install'
+                    sh 'npm install'
                 }
            }
         }
