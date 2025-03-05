3ebb234efcdbe58a385ba72b16ca0c7ad8c966b7
root <root@DF-VM.mshome.net>
2025-03-05 11:06:21 +0800

    modify name

--- zerozero-deploy.groovy
+++ zerozero-deploy.groovy
@@ -10,7 +10,7 @@ pipeline {
             }
            steps {
                 dir('zerozero-g2-react-develop') {
-                    sh 'echo "with docker."'
+                    sh 'node -v'
                 }
            }
         }
